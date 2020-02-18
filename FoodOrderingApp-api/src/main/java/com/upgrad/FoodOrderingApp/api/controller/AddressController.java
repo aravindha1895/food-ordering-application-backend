0859a0fd0a5ee9ff7a70c80d4292ff874fc092ca
Abhishek Aman <abhishek.aman@trakinvest.com>
2020-02-18 21:55:56 +0530

    added check in delete address api to validate same user

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/AddressController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/AddressController.java
@@ -110,6 +110,14 @@ public class AddressController {
             bearerToken = accessToken;
         }
 
+        CustomerEntity customerEntity = customerService.getCustomerByToken(bearerToken);
+
+        for(AddressEntity address : customerEntity.getAddress()){
+            if(!address.getUuid().equalsIgnoreCase(address_id))
+                throw new AddressNotFoundException("ATHR-004",
+                        "You are not authorized to view/update/delete any one else's address");
+            }
+
         AddressEntity deletedEntity = addressService.deleteAddressById(address_id);
 
         return new ResponseEntity<DeleteAddressResponse>(
