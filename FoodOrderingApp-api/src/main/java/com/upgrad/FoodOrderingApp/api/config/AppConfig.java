bcd510883f95e7924223d62433caa262c9dbdca7
Rutul Shah <rutulshah2007@gmail.com>
2020-02-19 17:01:39 +0530

    Removed state url from interceptor since login is not rquired for it

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/config/AppConfig.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/config/AppConfig.java
@@ -1,15 +1,14 @@
 package com.upgrad.FoodOrderingApp.api.config;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import com.upgrad.FoodOrderingApp.api.interceptor.AuthenticationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import com.upgrad.FoodOrderingApp.api.interceptor.AuthenticationInterceptor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class AppConfig extends WebMvcConfigurerAdapter {
@@ -19,7 +18,7 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 
 	/*Add all paths that require authentication checks here*/
 	String[] pathPatternToIntercept = { "/order/coupon/{coupon_name}", "/order", "/customer/logout", "/customer",
-			"/customer/password","/address","/address/customer","/address/{address_id}","/states" };
+			"/customer/password","/address","/address/customer","/address/{address_id}"};
 	List<String> pathsToIntercept = new ArrayList<String>(Arrays.asList(pathPatternToIntercept));
 
 	@Override
